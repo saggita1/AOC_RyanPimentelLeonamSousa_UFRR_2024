8a1a3675feef36bd02562b373e4678a1ef1ab80c
Leonam De Sousa Silva <127138174+leonamdesousa@users.noreply.github.com>
2025-02-21 10:37:15 -0400

    instrucao BEQ

--- processador/MuxULA.vhd
+++ processador/MuxULA.vhd
@@ -1,3 +1,4 @@
+-- MULTIPLEXADOR
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
@@ -16,32 +17,34 @@ entity MuxULA is
 end MuxULA;
 
 architecture Behavioral of MuxULA is
-    signal imm_8  : STD_LOGIC_VECTOR(7 downto 0);
-    signal jump_8 : STD_LOGIC_VECTOR(7 downto 0);
     constant zero_8 : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
 begin
-    imm_8  <= "00000" & imm;   -- Converte 3 bits em 8 bits
-    jump_8 <= "000"   & jump5;  -- Converte 5 bits em 8 bits
-
-    process(opcode, reg_data1, reg_data2, imm_8, jump_8)
+    process(opcode, reg_data1, reg_data2, imm, jump5)
+        variable imm_8  : STD_LOGIC_VECTOR(7 downto 0);
+        variable jump_8 : STD_LOGIC_VECTOR(7 downto 0);
     begin
+        imm_8  := "00000" & imm;   -- Converte o imediato de 3 bits para 8 bits
+        jump_8 := "000"   & jump5;  -- Converte o campo de salto de 5 bits para 8 bits
         case opcode is
-            when "000" | "001" => -- R-type: ADD ou SUB
+            when "000" | "001" =>  -- R-type: ADD ou SUB
                 alu_a <= reg_data1;
                 alu_b <= reg_data2;
-            when "010" | "011" => -- LW ou SW: usa somente o imediato
+            when "010" =>  -- LW: utiliza o imediato para formar o endereço
                 alu_a <= zero_8;
                 alu_b <= imm_8;
-            when "100" => -- ADDI
-                alu_a <= reg_data1;
-                alu_b <= imm_8;
-            when "101" => -- LI
+            when "011" =>  -- SW: idem LW
                 alu_a <= zero_8;
                 alu_b <= imm_8;
-            when "110" => -- BEQ
+            when "100" =>  -- ADDI: soma registrador com imediato
                 alu_a <= reg_data1;
-                alu_b <= zero_8;
-            when "111" => -- JUMP
+                alu_b <= imm_8;
+            when "101" =>  -- LI: carrega imediato
+                alu_a <= zero_8;
+                alu_b <= imm_8;
+            when "110" =>  -- BEQ: compara o registrador (Rdest) com R0 (conteúdo de reg_data2)
+                alu_a <= reg_data1;
+                alu_b <= reg_data2;
+            when "111" =>  -- JUMP: utiliza o campo de salto
                 alu_a <= zero_8;
                 alu_b <= jump_8;
             when others =>
