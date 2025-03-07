72fccce90bdfc1621c01f8e78863e45de8b19881
Ryan Pimentel <133511411+saggita1@users.noreply.github.com>
2025-03-07 04:21:04 -0400

    testbench

--- processador/tb_cpu.vhd
+++ processador/tb_cpu.vhd
@@ -1,61 +1,53 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity tb_CPU is
-end tb_CPU;
+entity tb_cpu is
+end tb_cpu;
 
-architecture sim of tb_CPU is
-    signal clk         : std_logic := '0';
-    signal pc_out      : std_logic_vector(7 downto 0);
-    signal instr_out   : std_logic_vector(7 downto 0);
-    signal alu_out     : std_logic_vector(7 downto 0);
-    signal ram_addr_out: std_logic_vector(7 downto 0);
-    signal r0_out, r1_out, r2_out, r3_out : std_logic_vector(7 downto 0);
+architecture Behavioral of tb_cpu is
+    signal clk_tb        : STD_LOGIC := '0';
+    signal pc_out_tb     : STD_LOGIC_VECTOR(7 downto 0);
+    signal instr_out_tb  : STD_LOGIC_VECTOR(7 downto 0);
+    signal alu_out_tb    : STD_LOGIC_VECTOR(7 downto 0);
+    signal ram_addr_out_tb : STD_LOGIC_VECTOR(7 downto 0);
+    signal r0_out_tb, r1_out_tb, r2_out_tb, r3_out_tb : STD_LOGIC_VECTOR(7 downto 0);
 
-    constant CLOCK_PERIOD : time := 20 ns;
-    constant NUM_CICLOS : integer := 20;
+    constant clk_period : time := 20 ns;
 
 begin
-    UUT: entity work.CPU
+    DUT : entity work.CPU
         port map(
-            clk          => clk,
-            pc_out       => pc_out,
-            instr_out    => instr_out,
-            alu_out      => alu_out,
-            ram_addr_out => ram_addr_out,
-            r0_out       => r0_out,
-            r1_out       => r1_out,
-            r2_out       => r2_out,
-            r3_out       => r3_out
+            clk          => clk_tb,
+            pc_out       => pc_out_tb,
+            instr_out    => instr_out_tb,
+            alu_out      => alu_out_tb,
+            ram_addr_out => ram_addr_out_tb,
+            r0_out       => r0_out_tb,
+            r1_out       => r1_out_tb,
+            r2_out       => r2_out_tb,
+            r3_out       => r3_out_tb
         );
-    clock_gen : process
-        variable i : integer := 0;
+
+    clk_process :process
     begin
-        while i < NUM_CICLOS loop
-            clk <= '0';
-            wait for CLOCK_PERIOD/2;
-            clk <= '1';
-            wait for CLOCK_PERIOD/2;
-            i := i + 1;
+        while now < 500 ns loop 
+            clk_tb <= '0';
+            wait for clk_period/2;
+            clk_tb <= '1';
+            wait for clk_period/2;
         end loop;
-        
         wait;
     end process;
 
-    monitor : process(clk)
+    stimulus_process: process
     begin
-        if rising_edge(clk) then
-            report 
-                "PC = " & to_hstring(pc_out)      &
-                " | Instr = " & to_hstring(instr_out) &
-                " | ALU = " & to_hstring(alu_out) &
-                " | RAM_addr = " & to_hstring(ram_addr_out) &
-                " | R0 = " & to_hstring(r0_out)   &
-                " | R1 = " & to_hstring(r1_out)   &
-                " | R2 = " & to_hstring(r2_out)   &
-                " | R3 = " & to_hstring(r3_out);
-        end if;
+        wait for 100 ns;  
+        wait for 400 ns; 
+
+        assert false report "Fim da simulação" severity failure;
+
     end process;
 
-end sim;
+end Behavioral;
